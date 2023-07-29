1523599fbd18ece11e3a68e38637ef9cefd85694
Matthew Kandiah <matthew.kandiah@glean.co>
2023-07-29 16:58:45 +0100

    one logic error fixed - still one test failing

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -7,26 +7,24 @@ Token Lexer::getNextToken() {
 	// skips whitespace
 	while (std::isspace(lastCharacter)) {
 		lastCharacter = characterStream -> get();
-
-		switch (lastCharacter) {
-			case '=': return Token(TokenType::equals, "");
-			case '+': return Token(TokenType::plus, "");
-			case '-': return Token(TokenType::minus, "");
-			case '(': return Token(TokenType::leftBracket, "");
-			case ')': return Token(TokenType::rightBracket, "");
-		}
-
-		if (std::isalpha(lastCharacter)) {
-			// could be an identifier or a keyword
-		}
-
-		if (std::isdigit(lastCharacter)) {
-			// got the start of a number
-		}
-
-		return Token(TokenType::illegal, std::string(1, lastCharacter));
 	}
 
-	return Token(TokenType::eof, "");
+	switch (lastCharacter) {
+		case '=': return Token(TokenType::equals, "");
+		case '+': return Token(TokenType::plus, "");
+		case '-': return Token(TokenType::minus, "");
+		case '(': return Token(TokenType::leftBracket, "");
+		case ')': return Token(TokenType::rightBracket, "");
+	}
+
+	if (std::isalpha(lastCharacter)) {
+		// could be an identifier or a keyword
+	}
+
+	if (std::isdigit(lastCharacter)) {
+		// got the start of a number
+	}
+
+	return Token(TokenType::illegal, std::string(1, lastCharacter));
 }
 
