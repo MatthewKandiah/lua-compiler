337739bf0a6b16ab49d7e6b12201c211e22103b0
Matthew Kandiah <matthew.kandiah@glean.co>
2023-07-29 16:44:44 +0100

    working tests for single character reading, failing test for multiple character reading, looks like they all get eaten by the first call

--- test/lexer.test.cpp
+++ test/lexer.test.cpp
@@ -9,6 +9,53 @@ TEST(LexerTests, ShouldReturnEqualsToken) {
 
 	auto result = lexer.getNextToken();
 
-	EXPECT_EQ(result.type, TokenType::equals); // failing - getting an illegal token instead
+	EXPECT_EQ(result.type, TokenType::equals);
+}
+
+TEST(LexerTests, ShouldReturnPlusToken) {
+	std::istringstream inputStream {"+"};
+	Lexer lexer(&inputStream);
+
+	auto result = lexer.getNextToken();
+
+	EXPECT_EQ(result.type, TokenType::plus);
+}
+
+TEST(LexerTests, ShouldReturnMinusToken) {
+	std::istringstream inputStream {"-"};
+	Lexer lexer(&inputStream);
+
+	auto result = lexer.getNextToken();
+
+	EXPECT_EQ(result.type, TokenType::minus);
+}
+
+TEST(LexerTests, ShouldReturnLeftBracketToken) {
+	std::istringstream inputStream {"("};
+	Lexer lexer(&inputStream);
+
+	auto result = lexer.getNextToken();
+
+	EXPECT_EQ(result.type, TokenType::leftBracket);
+}
+
+TEST(LexerTests, ShouldReturnRightBracketToken) {
+	std::istringstream inputStream {")"};
+	Lexer lexer(&inputStream);
+
+	auto result = lexer.getNextToken();
+
+	EXPECT_EQ(result.type, TokenType::rightBracket);
+}
+
+TEST(LexerTests, ShouldConsumeTokensAsTheyAreRead) {
+	std::istringstream inputStream {"=+-()"};
+	Lexer lexer(&inputStream);
+
+	EXPECT_EQ(lexer.getNextToken().type, TokenType::equals);
+	EXPECT_EQ(lexer.getNextToken().type, TokenType::plus);
+	EXPECT_EQ(lexer.getNextToken().type, TokenType::minus);
+	EXPECT_EQ(lexer.getNextToken().type, TokenType::leftBracket);
+	EXPECT_EQ(lexer.getNextToken().type, TokenType::rightBracket);
 }
 
