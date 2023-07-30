829f0d8461d7dadc66e0bb4258f760d35a4cb6b8
Matthew Kandiah <matthew.kandiah@glean.co>
2023-07-30 21:07:44 +0100

    set up for parser tests

---
+++ test/parser.test.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+#include "../src/parser.h"
+
+// TODO - add a mock lexer so we can pass an arbitrary series of tokens into the parser for testing
+
+TEST(ParserTests, TestThatShouldPass) {
+  EXPECT_EQ(1, 1);
+}
