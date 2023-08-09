c159b211719373b58171f455456afe436ea0a915
Matthew Kandiah <matthew.kandiah@glean.co>
2023-08-09 21:49:48 +0100

    split parser into base, actual, and mock classes. Hopefully simplify the next load of tests

--- src/parser.h
+++ src/parser.h
@@ -8,22 +8,35 @@
 #include <cstdint>
 #include <istream>
 #include <memory>
+#include <vector>
 
-class Parser {
+class BaseParser {
+public:
+  virtual std::unique_ptr<AstExpressionNode> parseExpression() = 0;
+};
+
+class Parser : public BaseParser {
 public:
   Parser(std::unique_ptr<BaseLexer> l)
       : lexerPtr{std::move(l)}, currentToken{lexerPtr->getNextToken()} {};
+  std::unique_ptr<AstExpressionNode> parseExpression() override;
 
+private:
   std::unique_ptr<BaseLexer> lexerPtr;
   Token currentToken;
-  Token getNextToken();
-  std::unique_ptr<AstExpressionNode> parseExpression();
   std::unique_ptr<AstExpressionNode> parsePrimaryExpression();
   std::unique_ptr<AstExpressionNode>
       parseBinaryExpressionRhs(std::int64_t,
                                std::unique_ptr<AstExpressionNode>);
   std::int64_t getOperatorPrecedence(Token);
   std::string tokenTypeToBinaryOperatorString(TokenType);
+};
+
+class MockParser : public BaseParser {
+public:
+    MockParser() {
+        // TODO - work out how we're going to pass in our nodes and return them sensibly for tests
+    };
 };
 
 #endif // !PARSER_H
