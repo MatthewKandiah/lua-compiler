024b417e672aa4db5c1563d1a981e80b40159d65
Matthew Kandiah <matthew.kandiah@glean.co>
2023-08-09 08:47:44 +0100

    make a mock lexer class, preparation to simplify parser tests

--- src/lexer.h
+++ src/lexer.h
@@ -1,17 +1,30 @@
 #ifndef LEXER_H
 #define LEXER_H
 #include <istream>
+#include <vector>
 
 #include "token.h"
 
-class Lexer {
+class BaseLexer {
+public: 
+  virtual Token getNextToken() = 0;
+};
+
+class Lexer : public BaseLexer {
 public:
-  Lexer(std::istream& inputStream): inputStreamPtr {&inputStream} {};
-  Token getNextToken();
+  Lexer(std::istream& inputStream): inputStreamPtr {&inputStream} {}
+  Token getNextToken() override;
 
 private:
   std::istream* inputStreamPtr;
   char lastCharacter{' '};
+};
+
+class MockLexer : public BaseLexer {
+public:
+  MockLexer(std::vector<Token> t): tokens{std::move(t)} {}
+  int nextTokenIndex = 0;
+  std::vector<Token> tokens;
 };
 
 #endif // !LEXER_H
